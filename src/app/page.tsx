20cd0eecd4e1e1c6ef46ae60ee4e132c5484608a
matheus jesse <matheus_jesse@hotmail.com>
2024-01-13 20:58:51 -0300

    style: estilizando o input do form de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,16 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='flex flex-wrap w-80 bg-white h-96 shadow-md'>
+      <div className=' border flex flex-wrap w-80 bg-white h-96 shadow-md rounded-lg'>
         <form className=" flex flex-wrap justify-center items-center content-center">
-          <h1 className="text-center w-80">Login</h1>
-          <label htmlFor="email" className="w-72 h-11 flex flex-col">
+          <h1 className="text-center w-80 mb-12">Login</h1>
+          <label htmlFor="email" className="w-72 h-18 flex flex-col mb-6">
             Email
-            <input className="shadow-sm w-72 h-8" type='text' name="email" id="email"/>
+            <input className="shadow-sm border w-72 h-9 mt-2 rounded" type='text' name="email" id="email"/>
           </label>
-          <label htmlFor="password" className="w-72 h-11 flex flex-col">
+          <label htmlFor="password" className="w-72 h-18 flex flex-col mb-6">
             Senha
-            <input className="shadow-sm w-72 h-8" type='text' name="password" id="password"/>
+            <input className="shadow-sm border w-72 h-9 mt-2 rounded" type='text' name="password" id="password"/>
           </label>
           <button>Entrar</button>
         </form>
