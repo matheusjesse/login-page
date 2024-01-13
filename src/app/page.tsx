28e3c6dd02c9f3053329cc34194efc102ba19a83
matheus jesse <matheus_jesse@hotmail.com>
2024-01-13 20:32:29 -0300

    style: stilo inicial do label e input com tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Oi</h1>
+      <div className='flex flex-wrap w-80 bg-white h-96 shadow-md'>
+        <form className=" flex flex-wrap justify-center items-center content-center">
+          <h1 className="text-center w-80">Login</h1>
+          <label htmlFor="email" className="w-72 h-11 flex flex-col">
+            Email
+            <input className="shadow-sm w-72 h-8" type='text' name="email" id="email"/>
+          </label>
+          <label htmlFor="password" className="w-72 h-11 flex flex-col">
+            Senha
+            <input className="shadow-sm w-72 h-8" type='text' name="password" id="password"/>
+          </label>
+          <button>Entrar</button>
+        </form>
+      </div>
     </main>
   )
 }
