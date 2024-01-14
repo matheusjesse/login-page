1b1ac00ad5136e48054a6b791e4ee59f46307f9f
matheus jesse <matheus_jesse@hotmail.com>
2024-01-13 21:07:04 -0300

    style: adicionando estilo no botão de login e alterando a cor do texto do form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className=' border flex flex-wrap w-80 bg-white h-96 shadow-md rounded-lg'>
         <form className=" flex flex-wrap justify-center items-center content-center">
-          <h1 className="text-center w-80 mb-9">Login</h1>
-          <label htmlFor="email" className="w-72 h-18 flex flex-col mb-6">
+          <h1 className="text-slate-700 text-center w-80 mb-9">Login</h1>
+          <label htmlFor="email" className="text-slate-700 w-72 h-18 flex flex-col mb-6">
             Email
             <input className="shadow-sm border w-72 h-9 mt-2 rounded" type='text' name="email" id="email"/>
           </label>
-          <label htmlFor="password" className="w-72 h-18 flex flex-col mb-6">
+          <label htmlFor="password" className="text-slate-700 w-72 h-18 flex flex-col mb-6">
             Senha
             <input className="shadow-sm border w-72 h-9 mt-2 rounded" type='text' name="password" id="password"/>
           </label>
-          <button>Entrar</button>
+          <button className="bg-indigo-500 rounded text-white w-40 h-8">Entrar</button>
         </form>
       </div>
     </main>
